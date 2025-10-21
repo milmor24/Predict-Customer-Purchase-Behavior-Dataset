7aba3dcb112cb3bc9bd539c515eeca214cae991f
milmor24 <milmor24@student.hh.se>
2025-10-21 17:05:34 +0200

    Added customer_purchase_data and PY FILE

---
+++ group_project.py
@@ -0,0 +1,31 @@
+import pandas as pd
+print("---MENU FOR DATA ANALYSTICS---")
+
+def load_dataset():
+    ds_name = input("Type the name of the dataset (CSV): ").strip()
+    try:
+         df
+
+
+def train_classification_moduke():
+      
+def evaluate_save_perfomance():
+     None
+      
+def simulate_enviroment():
+     None
+
+      
+
+
+
+
+while True:
+    print("Choose a option\n(1) Load Dataset\n(2) Train a Classification Model\n(3) Evaluate and Save the Performance\n(4) Simulate enviroment")
+    option = input("Choose a option: ").strip()
+
+    if option == 1:
+          df = load_dataset
+
+
+
