7f82c40d66adc7f578c6afa00785cd3f82d31570
eliaz6z <mehrpuria@hotmail.com>
2025-10-21 18:09:30 +0200

    deleted test code

--- TEST_CODE.py
+++
@@ -1,8 +0,0 @@
-int(input("Choose a option: ").strip())
-
-while True:
-    print("Choose a option\n(1) Load Dataset\n(2) Train a Classification Model\n(3) Evaluate and Save the Performance\n(4) Simulate enviroment")
-    option = int(input("Choose a option: ").strip())
-
-    if option == 1:
-          df = load_dataset
