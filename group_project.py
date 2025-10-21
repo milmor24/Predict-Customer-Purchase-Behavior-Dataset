2ed17eacc5ae12267e42506577275e70a7e4ea9f
milmor24 <milmor24@student.hh.se>
2025-10-21 17:12:52 +0200

    Added testcode file

--- group_project.py
+++ group_project.py
@@ -1,31 +1,29 @@
 import pandas as pd
+df = None
 print("---MENU FOR DATA ANALYSTICS---")
 
 def load_dataset():
-    ds_name = input("Type the name of the dataset (CSV): ").strip()
-    try:
-         df
+     ds_name = input("Type the name of the dataset (CSV): ").strip()
+     try:
+          df = pd.read_csv(ds_name)
+     except:
 
 
-def train_classification_moduke():
+def train_classification_moduke(df):
       
-def evaluate_save_perfomance():
+def evaluate_save_perfomance(df):
+
+def simulate_enviroment(df):
      None
-      
-def simulate_enviroment():
-     None
 
       
-
-
-
 
 while True:
     print("Choose a option\n(1) Load Dataset\n(2) Train a Classification Model\n(3) Evaluate and Save the Performance\n(4) Simulate enviroment")
-    option = input("Choose a option: ").strip()
+    option = int(input("Choose a option: ").strip())
 
     if option == 1:
-          df = load_dataset
+          df = load_dataset()
 
 
 
