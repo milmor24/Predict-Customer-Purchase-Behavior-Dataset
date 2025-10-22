703e57ee4195de5ed0ae0a76e3597ce6a2d1bc47
milmor24 <milmor24@student.hh.se>
2025-10-22 08:56:53 +0200

    Updated, Imported and changed the code

--- group_project.py
+++ group_project.py
@@ -1,4 +1,9 @@
 import pandas as pd
+from sklearn.model_selection import train_test_split 
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import accuracy_score 
+from sklearn.metrics import classification_report 
 df = None
 print("---MENU FOR DATA ANALYSTICS---")
 
@@ -6,13 +11,15 @@ def load_dataset():
      ds_name = input("Type the name of the dataset (CSV): ").strip()
      try:
           df = pd.read_csv(ds_name)
-          
-          print(df.head())
+          print(f"Loaded CSV file: {ds_name}")
+          print(f"This is the DH-Head \n{df.head()}")
           return df
-     except:
+     except FileNotFoundError:
+          print("File not found. Check the path and try again.")
 
 
-def train_classification_moduke(df):
+def train_classification_module(df):
+     print("Choose a module: \n(1) KNN\n(2)Decision Tree"
       
 def evaluate_save_perfomance(df):
 
@@ -22,11 +29,27 @@ def simulate_enviroment(df):
       
 
 while True:
-    print("Choose a option\n(1) Load Dataset\n(2) Train a Classification Model\n(3) Evaluate and Save the Performance\n(4) Simulate enviroment")
-    option = int(input("Choose a option: ").strip())
+    
+     print("Choose a option\n(1) Load Dataset\n(2) Train a Classification Model\n(3) Evaluate and Save the Performance\n(4) Simulate enviroment (5) Q for quit:")
+     option = input("Choose a option: ").strip().lower()
 
-    if option == 1:
+     if option == "1":
           df = load_dataset()
+     elif option == "2:
+          if df is None:
+               print("You have to load dataset first!")
+          else:
+               try:
+                    train_classification_module(df)
+               except: print("An error has accured")
+
+
+     elif option == "q":
+          print("You have exited the program")
+          break
+     else
+          print("Invalid option, try again")
+
 
 
 
